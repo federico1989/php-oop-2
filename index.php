b27f1af23f475247033fb3029bcd7765d056a5c2
Federico <federico.scarpati1989@gmail.com>
2021-02-16 15:24:50 +0100

    Primo commit
    Impostazione dell'esercizio

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+/** Users class
+ * @author Fede User user@gmail.com
+ * @copyright 2021 Fede User
+ */
+    class Users {
+        public $name;
+        public $surname;
+        public $age;
+        public $email;
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Php oop 2</title>
+    </head>
+    <body>
+
+    </body>
+</html>
